9e4557a97c0ec95976fe3e5436778b14567de990
Kristin Iacone <kiacone@gmail.com>
2018-04-23 20:55:41 -0400

    html files

---
+++ fromClassRepo/local.js
@@ -0,0 +1,10 @@
+$( document ).ready(function(){
+  $('.collapsible').collapsible();
+  $('.carousel').carousel();
+  $('.slider').slider();
+  $('.modal').modal();
+  $('.button-collapse').sideNav({
+        closeOnClick: true
+      }
+    );
+  });
